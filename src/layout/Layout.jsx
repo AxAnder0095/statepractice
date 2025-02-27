c3ed10010faafaa56abee85b0e24fb8488b6ee91
Frenguin <104327737+Frenguin@users.noreply.github.com>
2025-02-26 23:11:52 -0600

    practice

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import Home from "../pages/Home/Home.jsx";
-
+import './LayoutStyles.sass'
 
 function Layout(){
     return (
         <>
-            <div className='container-fluid' id='my-container'>
-                <Home/>
+            <div className='container-fluid d-flex align-items-center justify-content-center' id='my-container'>
+                <div>
+                    <Home/>
+                </div>
             </div>
         </>
     )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,33 @@
+import './HomeStyles.sass'
+import {useState} from 'react'
+
 function Home(){
 
+    const [state, setState] = useState({
+        name: '',
+        age: 0,
+        count: 0
+    })
+
+    function handleClick(){
+        setState({...state, name: 'Alex', age: 29})
+    }
+
+    // useEffect(() => {
+    //     setState({...state, count: state.count + 1})
+    // }, [state.count]);
 
     return (
         <>
-            hello
+            <div className='container d-flex justify-content-center align-items-center' id='home-container'>
+                <div className='' id='inner-content'>
+                    <h3>State Management Practice</h3>
+                    <p>Name: {state.name}</p>
+                    <p>Age: {state.age}</p>
+                    <p>Count: {state.count}</p>
+                    <button onClick={handleClick}>Click to update name and age</button>
+                </div>
+            </div>
         </>
     )
 }
