6b11468a833529c693d3b3416a556e95720011c2
Frenguin <104327737+Frenguin@users.noreply.github.com>
2025-02-27 18:20:50 -0600

    trying to get label to work

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,13 +4,18 @@ import {useState} from 'react'
 function Home(){
 
     const [state, setState] = useState({
-        name: '',
+        firstName: '',
+        lastName: '',
         age: 0,
         count: 0
     })
 
     function handleClick(){
-        setState({...state, name: 'Alex', age: 29})
+        setState({...state, count: state.count + 1})
+    }
+
+    function handleSubmit(){
+        console.log(state.firstName)
     }
 
     // useEffect(() => {
@@ -20,11 +25,37 @@ function Home(){
     return (
         <>
             <div className='container d-flex justify-content-center align-items-center' id='home-container'>
-                <div className='' id='inner-content'>
-                    <h3>State Management Practice</h3>
-                    <p>Name: {state.name}</p>
+                <div className='inner-content'>
+                    <h3>Form</h3>
+                    {/*<form onSubmit={handleSubmit}>*/}
+                    {/*    <label htmlFor={'firstName'}>First Name:*/}
+                    {/*        <input*/}
+                    {/*            type="text"*/}
+                    {/*            value={state.firstName}*/}
+                    {/*            onChange={(fName) => setState({...state, firstName: fName.target.value})}*/}
+                    {/*        />*/}
+                    {/*    </label>*/}
+                    {/*    <lable htmlFor={'lastName'}> Last name:*/}
+                    {/*        <input*/}
+                    {/*        type="text"*/}
+                    {/*        value={state.lastName}*/}
+                    {/*        onChange={(lName) => setState({...state, lastName: lName.target.value})}/>*/}
+                    {/*    </lable>*/}
+                    {/*    <label htmlFor={'age'}> Age:*/}
+                    {/*        <input*/}
+                    {/*        type="number"*/}
+                    {/*        value={state.age}*/}
+                    {/*        onChange={(pAge) => setState({...state, age: pAge.target.value})}/>*/}
+                    {/*    </label>*/}
+                    {/*    <input type="submit" />*/}
+                    {/*</form>*/}
+                </div>
+                <div className='inner-content'>
+                    <h3>Name and Age display</h3>
+                    <p>First Name: {state.firstName}</p>
+                    <p>Last Name: {state.lastName}</p>
                     <p>Age: {state.age}</p>
-                    <button onClick={handleClick}>Click to update name and age</button>
+                    <button onClick={handleClick}>Count: {state.count}</button>
                 </div>
             </div>
         </>
